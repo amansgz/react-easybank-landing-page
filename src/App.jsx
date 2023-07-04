3f537a9ee5764605c5448f59a96c70f23734c414
amansgz <amansgz.ar@gmail.com>
2023-07-03 22:34:04 -0300

    Clean initial files. Add assets and font

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,93 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      
     </>
   )
 }
 
 export default App
+/*
+  Home
+  About
+  Contact
+  Blog
+  Careers
+
+  Request Invite
+
+
+
+  Next generation digital banking
+
+  Take your financial life online. Your Easybank account will be a one-stop-shop 
+  for spending, saving, budgeting, investing, and much more.
+
+  Request Invite
+
+
+
+  Why choose Easybank?
+
+  We leverage Open Banking to turn your bank account into your financial hub. Control 
+  your finances like never before.
+
+  Online Banking
+  Our modern web and mobile applications allow you to keep track of your finances 
+  wherever you are in the world.
+
+  Simple Budgeting
+  See exactly where your money goes each month. Receive notifications when you’re 
+  close to hitting your limits.
+
+  Fast Onboarding
+  We don’t do branches. Open your account in minutes online and start taking control 
+  of your finances right away.
+
+  Open API
+  Manage your savings, investments, pension, and much more from one account. Tracking 
+  your money has never been easier.
+
+  
+
+  Latest Articles
+
+  By Claire Robinson
+  Receive money in any currency with no fees
+  The world is getting smaller and we’re becoming more mobile. So why should you be 
+  forced to only receive money in a single …
+
+  By Wilson Hutton
+  Treat yourself without worrying about money
+  Our simple budgeting feature allows you to separate out your spending and set 
+  realistic limits each month. That means you …
+
+  By Wilson Hutton
+  Take your Easybank card wherever you go
+  We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
+  while you’re abroad. We’ll even show you …
+
+  By Claire Robinson
+  Our invite-only Beta accounts are now live!
+  After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
+  It’s easy to request an invite through the site ...
+
+  
+
+  About Us
+  Contact
+  Blog
+  Careers
+  Support
+  Privacy Policy
+
+  Request Invite
+
+  © Easybank. All Rights Reserved
+*/
