8710bac0ec3f0c09f96766f6b5a2004c98df0587
amansgz <amansgz.ar@gmail.com>
2023-07-05 14:38:10 -0300

    Ad indexes ans basic styles

---
+++ src/assets/index.js
@@ -0,0 +1,41 @@
+import logo from "./logo.svg";
+import bgIntroDesktop from "./bg-intro-desktop.svg";
+import bgIntroMobile from "./bg-intro-mobile.svg";
+import iconHamburguer from "./icon-hamburger.svg";
+import iconClose from "./icon-close.svg";
+import iconOnline from "./icon-online.svg";
+import iconBudgeting from "./icon-budgeting.svg";
+import iconOnboarding from "./icon-onboarding.svg";
+import iconApi from "./icon-api.svg";
+import imgMockups from "./image-mockups.jpg";
+import imgCurrency from "./image-currency.jpg";
+import imgRestaurant from "./image-restaurant.jpg";
+import imgPlane from "./image-plane.jpg";
+import imgConfetti from "./image-confetti.jpg";
+import iconFacebook from "./icon-facebook.svg";
+import iconYoutube from "./icon-youtube.svg";
+import iconTwitter from "./icon-twitter.svg";
+import iconPinterest from "./icon-pinterest.svg";
+import iconInstagram from "./icon-instagram.svg";
+
+export {
+	logo,
+	bgIntroDesktop,
+	bgIntroMobile,
+	iconHamburguer,
+	iconClose,
+	iconOnline,
+	iconBudgeting,
+	iconOnboarding,
+	iconApi,
+	imgMockups,
+	imgCurrency,
+	imgRestaurant,
+	imgPlane,
+	imgConfetti,
+	iconFacebook,
+	iconYoutube,
+	iconTwitter,
+	iconPinterest,
+	iconInstagram
+}
---
+++ src/constants/index.js
@@ -0,0 +1,67 @@
+import { iconOnline, iconBudgeting, iconOnboarding, iconApi, imgCurrency, imgRestaurant, imgPlane, imgConfetti } from "../assets";
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: iconOnline,
+    title: "Online Banking",
+    content:
+      "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+  },
+  {
+    id: "feature-2",
+    icon: iconBudgeting,
+    title: "Simple Budgeting",
+    content:
+      "See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.",
+  },
+  {
+    id: "feature-3",
+    icon: iconOnboarding,
+    title: "Fast Onboarding",
+    content:
+      "We don’t do branches. Open your account in minutes online and start taking control of your finances right away.",
+  },
+   {
+    id: "feature-4",
+    icon: iconApi,
+    title: "Open API",
+    content:
+      "A balance transfer credit card can save you a lot of money in interest charges.",
+  },
+];
+
+export const articles = [
+  {
+    id: "article-1",
+    content:
+      "The world is getting smaller and we’re becoming more mobile. So why should you be   forced to only receive money in a single …",
+    author: "By Claire Robinson",
+    title: "Receive money in any currency with no fees",
+    img: imgCurrency,
+  },
+  {
+    id: "article-2",
+    content:
+      "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …",
+    author: "By Wilson Hutton",
+    title: "Treat yourself without worrying about money",
+    img: imgRestaurant,
+  },
+  {
+    id: "article-3",
+    content:
+      "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …",
+    author: "By Wilson Hutton",
+    title: "Take your Easybank card wherever you go",
+    img: imgPlane,
+  },
+  {
+    id: "article-4",
+    content:
+      "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...",
+    author: "By Claire Robinson",
+    title: "Our invite-only Beta accounts are now live!",
+    img: imgConfetti,
+  },
+];
