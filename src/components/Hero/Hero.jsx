b9df979904b015795e0f136d03cc306b7ad297dd
amansgz <amansgz.ar@gmail.com>
2023-07-20 17:39:07 -0300

    Add hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,26 @@
 import "./Hero.css";
-import Button from "../shared/Button";
+import Button from "../shared/Button/Button";
+import {imgMockups} from "../../assets";
+
 
 const Hero = () => (
-	<section className= "hero">
-		<div>
-			<h1>
-				Next generation digital banking
-			</h1>
-			<p className= "hero__description">	
-		  	Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
-		  </p>	
-			<Button />
-		</div>
-
-		<div className= "container-images">
-		</div>
+	<section aria-label= "welcome" className= "section-hero">
+		<div className= "container-hero">
+			<div className= "box">
+				<img className= "box__image"
+					src= {imgMockups} alt= "mockups devices"/>
+			</div>
+			
+			<div className= "hero-description">
+				<h1 className= "hero-description__title">
+					Next generation digital banking
+				</h1>
+				<p className= "hero-description__text">	
+			  	Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
+			  </p>	
+				<Button />
+			</div>
+		</div>	
 	</section>
 );
 
