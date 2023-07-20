fc13a2897b4e27d000c0910cedfb71a03f12cf73
amansgz <amansgz.ar@gmail.com>
2023-07-20 18:16:24 -0300

    Fix spaces

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,6 @@ import "./Hero.css";
 import Button from "../shared/Button/Button";
 import {imgMockups} from "../../assets";
 
-
 const Hero = () => (
 	<section aria-label= "welcome" className= "section-hero">
 		<div className= "container-hero">
@@ -16,8 +15,8 @@ const Hero = () => (
 					Next generation digital banking
 				</h1>
 				<p className= "hero-description__text">	
-			  	Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
-			  </p>	
+					Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
+				</p>
 				<Button />
 			</div>
 		</div>	
