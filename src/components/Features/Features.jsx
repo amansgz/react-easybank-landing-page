967d2f4e278a2d45a37ba837c599aa7e98eb199a
amansgz <amansgz.ar@gmail.com>
2023-07-20 17:39:55 -0300

    Add features section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -2,34 +2,34 @@ import "./Features.css";
 import { features } from "../../constants";
 
 const Features = () => (
-	<section className= "container">
-		<div className= "box">
-			<h2 className= "box__title">
+	<section className="section-features" aria-label= "why us">
+		<div>
+			<h2>
 				Why choose Easybank?
-			  </h2>
-			<p className= "box__description">	
+			 </h2>
+			<p className= "text">	
 	  		We leverage Open Banking to turn your bank account into your financial hub. Control 
 	  		your finances like never before.
 	  	</p>
-	  </div>
-
-		<div className= "container-features">
-		  {features.map((feature) => (
-		    <article key={feature.id} 
-		    	className= "feature">
-		    	<figure className= "feature__icon">
-		    		<img src= {feature.icon} />
-		    	</figure>	
-		      <h3 className= "feature__title">
-		        {feature.title}
-		      </h3>
-		      <p className= "feature__content">
-		        {feature.content}
-		      </p>
-		    </article>
-		  ))}
-		</div> 	
-  </section>		
+		  
+			<div className= "container-features">
+			  {features.map((feature) => (
+			    <article key={feature.id} 
+			    	className= "feature">
+			    	<figure className= "feature__icon">
+			    		<img src= {feature.icon} />
+			    	</figure>	
+			      <h3 className= "feature__title">
+			        {feature.title}
+			      </h3>
+			      <p className= "feature__text">
+			        {feature.content}
+			      </p>
+			    </article>
+			  ))}
+			</div>
+  	</div>
+  </section>
 )
 
 export default Features;
