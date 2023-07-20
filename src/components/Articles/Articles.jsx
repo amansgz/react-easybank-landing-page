d2d743b734fba94e49e405d22b71019213b9819c
amansgz <amansgz.ar@gmail.com>
2023-07-20 17:40:41 -0300

    Add articles section

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -2,35 +2,37 @@ import "./Articles.css";
 import { articles } from "../../constants";
 
 const Articles = () => (	
-	<section>
-		<h2 className= "section__title">
-			Latest Articles
-		</h2>
-		<div className= "container-articles">
-			{articles.map ((article)=> (
-				<article key= {article.id}
-					className= "card-article">
-					<picture>
-						<img className= "card-article__image"
-						src= {article.img} alt=""/>
-					</picture>
+	<section aria-label= "blog">
+		<div className= "container">
+			<h2 className= "container__title">
+				Latest Articles
+			</h2>
+			<div className= "container-cards">
+				{articles.map ((article)=> (
+					<article key= {article.id}
+						className= "card">
+						<picture>
+							<img className= "card__image"
+							src= {article.img} alt= "{article.alt}"/>
+						</picture>
 
-					<div className= "wrapper">
-						<span className= "card-article__author">
-							{article.author}
-						</span>
-						<h3 className= "card-article__link">
-							<a href="" >
-								{article.title}
-							</a>
-						</h3>
-						<p className= "card-article__content">
-							{article.content}
-						</p>
-					</div>	
-				</article>
-			))}
-		</div>	
+						<div className= "card-body">
+							<span>
+								{article.author}
+							</span>
+							<h3 className= "card-body__title">
+								<a href="" >
+									{article.title}
+								</a>
+							</h3>
+							<p className= "card-body__text">
+								{article.content}
+							</p>
+						</div>	
+					</article>
+				))}
+			</div>	
+		</div>
 	</section>
 );
 
