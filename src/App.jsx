75c63056a893799236f407c692561c0762272723
amansgz <amansgz.ar@gmail.com>
2023-07-24 17:35:59 -0300

    Review header with responsive navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import Header from "./components/Header/Header";
+/*
 import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features";
 import Articles from "./components/Articles/Articles";
 import Footer from "./components/Footer/Footer";
 
-
+*/
 function App() {
   return (
     <>
       <Header />
-      <main>
-        <Hero />
-        <Features />
-        <Articles />
-      </main>  
-      <Footer />
     </>
   )
 }
