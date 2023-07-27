d785288e5ef19f9ac1e56ffd1a84b537413875e0
amansgz <amansgz.ar@gmail.com>
2023-07-27 20:41:00 -0300

    Update styles. Update hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,18 +3,18 @@ import Button from "../shared/Button/Button";
 import {imgMockups} from "../../assets";
 
 const Hero = () => (
-	<section aria-label= "welcome" className= "section-hero">
-		<div className= "container-hero">
-			<div className= "box">
-				<img className= "box__image"
+	<section className= "hero" aria-label= "welcome">
+		<div className= "container-sm-grid-1 container-md-grid-2">
+			<div className= "item-image">
+				<img className= "hero__image"
 					src= {imgMockups} alt= "mockups devices"/>
 			</div>
 			
-			<div className= "hero-description">
-				<h1 className= "hero-description__title">
+			<div className= "wrapper item-text">
+				<h1 className= "hero__title">
 					Next generation digital banking
 				</h1>
-				<p className= "hero-description__text">	
+				<p className= "hero__text">	
 					Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.
 				</p>
 				<Button />
